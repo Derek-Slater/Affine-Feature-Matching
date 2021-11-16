cddb78135ede5efcc8bf43aa787d088cabb4fa26
Derek Slater <derekslater01@gmail.com>
2021-11-16 14:18:07 -0800

    Add project files.

---
+++ CSS487-Project/main.cpp
@@ -0,0 +1,21 @@
+// main.cpp
+// 
+// Author: Derek Slater, Shakeel Khan
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+using namespace cv;
+
+#include <iostream>
+using namespace std;
+
+// main:
+// preconditions: 
+// postconditions: 
+int main(int argc, char* argv[])
+{
+	
+	
+	return 0;
+}
